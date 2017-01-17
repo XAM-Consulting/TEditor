304cc8c929658a403c69302dad91ed999ae16372
Jesse Jiang <jessejiang0214@outlook.com>
2017-01-17 14:16:46 +1100

    Modify demo UI Upload images

--- Sample/TEditor.Forms.Sample/TEditor.Forms.Sample.cs
+++ Sample/TEditor.Forms.Sample/TEditor.Forms.Sample.cs
@@ -4,28 +4,28 @@ using Xamarin.Forms;
 
 namespace TEditor.Forms.Sample
 {
-	public class App : Application
-	{
-		public App ()
-		{
-			// The root page of your application
-			this.MainPage = new NavigationPage (new ContentPage{ Content = new TEditorHtmlView()});
-		}
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            this.MainPage = new NavigationPage(new ContentPage { Content = new TEditorHtmlView(), BackgroundColor = Color.White });
+        }
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
 
